8a6a8a1b265a37d8389e09e7b3cf0425190834e0
fidnd28 <hafidndiansyahxrpl2@gmail.com>
2025-02-08 18:38:36 +0700

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const messages = [
-    "Are you sure?",
+    "are u sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
+    "Beneran nih di tolak?",
+    "Sayang qilaa, please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
